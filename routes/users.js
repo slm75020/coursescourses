e41955df7681bb6fa68f9c1353a2654a653539b9
Salem Yahiaoui <slm75@hotmail.fr>
2018-03-21 19:11:40 +0100

    routes

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,29 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   // création d'un utilisateur
   // sauvegarde en base de données
-  console.log(req.body)
+  var userInfo = req.body;
+  var errors = [];
+
+  if (!validator.isEmail(userInfo.mail)){
+    errors.push("invalid email");
+  }
+  if (userInfo.prenom.length < 2 || userInfo.prenom.length > 25){
+    errors.push("votre nom doit contenir entre 2 et 15 characteres");
+  }
+  if (userInfo.nom.length < 2 || userInfo.nom.length > 25){
+    errors.push("votre nom doit contenir entre 2 et 15 characteres");
+  }
+  
+  if (userInfo.password != userInfo.password2){
+    errors.push("mot de passe non identiques");
+  }
+
+  if (errors.length > 0){
+    res.render({title:  errors: errors});
+  } else {
+    var encryptedPassword = await bcrypt.hash(userInfo.password, 10);res.send(t);
+    res.send(encryptedPassword);
+  }
   res.render('command', { title: 'Commandez votre course' });
 });
 
